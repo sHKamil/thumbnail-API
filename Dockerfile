3d54f8fa81955f77ec3f5b9fef81b231a0ef6ec9
sHKamil <91428301+sHKamil@users.noreply.github.com>
2023-08-09 23:29:32 +0200

    nginx fix

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,4 @@ RUN apt-get update; \
     apt-get install -y libmagickwand-dev; \
     pecl install imagick; \
     docker-php-ext-enable imagick;
+
--- app/index.php
+++ app/index.php
@@ -1,10 +1,11 @@
 <?php
+
 const BASE_PATH = __DIR__ . DIRECTORY_SEPARATOR;
 require_once BASE_PATH . 'functions.php';
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 spl_autoload_register('autoloader');
 
-use Controller\ThumbnailController;
+use Controller\ModeController;
 use Controller\UrlController;
 
 new ModeController(new UrlController($uri));
